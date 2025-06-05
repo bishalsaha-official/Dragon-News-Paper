04b455e904cd33ad838ca73e6486d55bd947efae
bishalsaha-official <cse.bishalsaha@gmail.com>
2025-06-05 23:10:17 +0600

    Implement Navbar Components

--- src/layouts/HomeLayouts.jsx
+++ src/layouts/HomeLayouts.jsx
@@ -1,17 +1,24 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayouts = () => {
     return (
-        <div>
+        <div className="font-poppins max-w-screen-xl mx-auto">
             <header>
                 <Header></Header>
                 <section className="max-w-7xl mx-auto bg-[#F3F3F3] rounded p-4">
                     <LatestNews></LatestNews>
                 </section>
             </header>
-            <nav></nav>
-            <main></main>
+            <nav className="mt-7">
+                <Navbar></Navbar>
+            </nav>
+            <main className="mt-7 grid md:grid-cols-12 gap-4">
+                <aside className="col-span-3">left nav</aside>
+                <section className="col-span-6">center</section>
+                <aside className="col-span-3">righ nav</aside>
+            </main>
         </div>
     );
 };
